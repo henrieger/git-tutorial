a227a612d2cc276245314a6d7f18638e58923832
Henrique Rieger <henriquerieger2001@gmail.com>
2026-02-24 15:55:29 -0300

    fix deliberate mistakes in anoles.R

--- anoles.R
+++ anoles.R
@@ -22,13 +22,13 @@ tree<-read.tree("Anolis.tre")
 plotTree(tree,type="fan",ftype="i")
 
 ## read data into a vector
-svl<-read.csv("svll.csv",row.names=1)
+svl<-read.csv("svl.csv",row.names=1)
 svl<-setNames(svl$svl,rownames(svl))
 svl
 
 # estimate ancestral states with variances and confidence intervals
 fit<-fastAnc(tree,svl,vars=TRUE,CI=TRUE)
-printf(fit,printlen=10)
+print(fit,printlen=10)
 
 ## as discussed in class, 95% CIs can be broad
 range(svl) ## compare to root node
@@ -36,4 +36,4 @@ range(svl) ## compare to root node
 ## projection of the reconstruction onto the edges of the tree
 obj<-contMap(tree,svl,plot=FALSE)
 obj
-plot(object, type="fan", legend=0.7*max(nodeHeights(tree)))
+plot(obj, type="fan", legend=0.7*max(nodeHeights(tree)))
